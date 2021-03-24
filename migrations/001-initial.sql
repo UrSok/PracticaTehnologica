5083948addaae39bc66c70e536caa278073b4a20
Ur Sok <marin.dabija@mail.ru>
2021-03-24 23:56:25 +0200

    Removed unnecessary not nulls for primary keys

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -1,24 +1,20 @@
 CREATE TABLE Library (
-      id   INTEGER PRIMARY KEY AUTOINCREMENT
-                   NOT NULL,
+      id   INTEGER PRIMARY KEY AUTOINCREMENT,
       path VARCHAR NOT NULL
     );
 	
 CREATE TABLE Music (
-    id       INTEGER PRIMARY KEY AUTOINCREMENT
-                     NOT NULL,
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
     src      VARCHAR NOT NULL,
     src_type INT     NOT NULL
                      DEFAULT (0) 
 );
 CREATE TABLE Playlist (
-      id  INTEGER PRIMARY KEY AUTOINCREMENT
-                  NOT NULL,
+      id  INTEGER PRIMARY KEY AUTOINCREMENT,
       name VARCHAR NOT NULL
     );
 CREATE TABLE PlaylistMusic (
-      id          INTEGER PRIMARY KEY AUTOINCREMENT
-                          NOT NULL,
+      id          INTEGER PRIMARY KEY AUTOINCREMENT,
       playlist_id INTEGER REFERENCES Playlist (id)
                           NOT NULL,
       music_id    INTEGER REFERENCES Music (id)
