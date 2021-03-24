77e52dcd8fdc5bdce7486cac15eeaa840b211317
Ur Sok <marin.dabija@mail.ru>
2021-03-24 18:24:05 +0200

    Added src_type to Music table

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -1,20 +1,22 @@
-CREATE TABLE IF NOT EXISTS Library (
+CREATE TABLE Library (
       id   INTEGER PRIMARY KEY AUTOINCREMENT
                    NOT NULL,
       path VARCHAR NOT NULL
     );
 	
-CREATE TABLE IF NOT EXISTS Music (
-      id  INTEGER PRIMARY KEY AUTOINCREMENT
-                  NOT NULL,
-      src VARCHAR NOT NULL
-    );
-CREATE TABLE IF NOT EXISTS Playlist (
+CREATE TABLE Music (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT
+                     NOT NULL,
+    src      VARCHAR NOT NULL,
+    src_type INT     NOT NULL
+                     DEFAULT (0) 
+);
+CREATE TABLE Playlist (
       id  INTEGER PRIMARY KEY AUTOINCREMENT
                   NOT NULL,
       name VARCHAR NOT NULL
     );
-CREATE TABLE IF NOT EXISTS PlaylistMusic (
+CREATE TABLE PlaylistMusic (
       id          INTEGER PRIMARY KEY AUTOINCREMENT
                           NOT NULL,
       playlist_id INTEGER REFERENCES Playlist (id)
