fb0e35dfcbfd7b110413e332bd67d3aa50db22d8
Ur Sok <marin.dabija@mail.ru>
2021-04-05 19:04:48 +0300

    Updated initial migration

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -1,22 +1,30 @@
 CREATE TABLE Library (
-      id   INTEGER PRIMARY KEY AUTOINCREMENT,
-      path VARCHAR NOT NULL
+      id     INTEGER PRIMARY KEY AUTOINCREMENT,
+      path   VARCHAR NOT NULL,
+      active BOOLEAN DEFAULT (1)
+                     NOT NULL
     );
-	
+
 CREATE TABLE Music (
     id       INTEGER PRIMARY KEY AUTOINCREMENT,
     src      VARCHAR NOT NULL,
     src_type INT     NOT NULL
-                     DEFAULT (0) 
+                     DEFAULT (0),
+    added     DATETIME DEFAULT(CURRENT_TIMESTAMP)
+                     NOT NULL
 );
 CREATE TABLE Playlist (
-      id  INTEGER PRIMARY KEY AUTOINCREMENT,
-      name VARCHAR NOT NULL
+      id      INTEGER PRIMARY KEY AUTOINCREMENT,
+      name    VARCHAR NOT NULL,
+      created DATETIME DEFAULT(CURRENT_TIMESTAMP)
+                      NOT NULL
     );
 CREATE TABLE PlaylistMusic (
       id          INTEGER PRIMARY KEY AUTOINCREMENT,
       playlist_id INTEGER REFERENCES Playlist (id)
                           NOT NULL,
       music_id    INTEGER REFERENCES Music (id)
+                          NOT NULL,
+      added       DATETIME DEFAULT(CURRENT_TIMESTAMP)
                           NOT NULL
     );
