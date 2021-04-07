ba3b8102ac256f1127264a2f6a8641a3480621ac
Ur Sok <marin.dabija@mail.ru>
2021-04-07 16:18:23 +0300

    Updated the title bar to use custom-title-bar library

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import { remote } from 'electron';
+import { Titlebar, Color } from 'custom-electron-titlebar';
 import App from './App';
 
-const win = remote.getCurrentWindow();
-
-window.onbeforeunload = () => {
-  win.removeAllListeners();
-};
-
-function handleWindowControls() {
-  // Make minimise/maximise/restore/close buttons work when they are clicked
-  document!.getElementById('min-button').addEventListener('click', () => {
-    win.minimize();
-  });
-
-  document.getElementById('max-button').addEventListener('click', () => {
-    win.maximize();
-  });
-
-  document.getElementById('restore-button').addEventListener('click', () => {
-    win.unmaximize();
-  });
-
-  document.getElementById('close-button').addEventListener('click', () => {
-    win.close();
-  });
-
-  function toggleMaxRestoreButtons() {
-    if (win.isMaximized()) {
-      document.body.classList.add('maximized');
-    } else {
-      document.body.classList.remove('maximized');
-    }
-  }
-
-  // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
-  toggleMaxRestoreButtons();
-  win.on('maximize', toggleMaxRestoreButtons);
-  win.on('unmaximize', toggleMaxRestoreButtons);
-}
-
-document.onreadystatechange = () => {
-  if (document.readyState === 'complete') {
-    handleWindowControls();
-  }
-};
+// eslint-disable-next-line no-new
+new Titlebar({
+  backgroundColor: Color.TRANSPARENT,
+});
 
 render(
   <HashRouter>
