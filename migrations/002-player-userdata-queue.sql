19809191b5f8f5e79e9d92c11deb2c794602ad87
Ur Sok <marin.dabija@mail.ru>
2021-04-10 01:31:18 +0300

    Updated Player with playingQueueEntryId

--- migrations/002-player-userdata-queue.sql
+++ migrations/002-player-userdata-queue.sql
@@ -1,20 +1,20 @@
-CREATE TABLE Queue (
+CREATE TABLE QueueEntry (
     id      INTEGER PRIMARY KEY AUTOINCREMENT,
     musicId INTEGER REFERENCES Music (id)
                     NOT NULL
 );
 
 CREATE TABLE Player (
-    shuffle         BOOLEAN        NOT NULL,
-    repeat          INT            NOT NULL
-                                   CHECK (repeat BETWEEN 0 AND 2),
-    playingMusicId  INT            REFERENCES Music (id),
-    playingFromType INTEGER        CHECK (playingFromType >= 0),
-    playingFromId   INTEGER,
-    played          DECIMAL (8, 7) NOT NULL
-                                   CHECK (played BETWEEN 0 AND 1),
-    volume          DECIMAL (3, 2) NOT NULL
-                                   CHECK (volume BETWEEN 0 AND 1)
+    shuffle             BOOLEAN        NOT NULL,
+    repeat              INT            NOT NULL
+                                       CHECK (repeat BETWEEN 0 AND 2),
+    playingQueueEntryId INT            REFERENCES QueueEntry (id),
+    playingFromType     INTEGER        CHECK (playingFromType >= 0),
+    playingFromId       INTEGER,
+    played              DECIMAL (8, 7) NOT NULL
+                                       CHECK (played BETWEEN 0 AND 1),
+    volume              DECIMAL (3, 2) NOT NULL
+                                       CHECK (volume BETWEEN 0 AND 1)
 );
 
 CREATE TABLE UserData (
