cd60b52130985a59249f3942b4319b125616be3f
Ur Sok <marin.dabija@mail.ru>
2021-04-09 21:56:29 +0300

    Fixed white screen for react-refresh??

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,23 @@ import { render } from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Titlebar, Color } from 'custom-electron-titlebar';
 import App from './App';
+import { store, StoreContext } from './utils/StoreContext';
 
-// eslint-disable-next-line no-new
-new Titlebar({
-  backgroundColor: Color.TRANSPARENT,
-});
+let titlebar: Titlebar | undefined;
+
+window.onload = () => {
+  if (titlebar === undefined) {
+    titlebar = new Titlebar({
+      backgroundColor: Color.TRANSPARENT,
+    });
+  }
+};
 
 render(
   <HashRouter>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   </HashRouter>,
   document.getElementById('root')
 );
