d72311d75fd5219a525f91c2087ec68128822827
Ur Sok <marin.dabija@mail.ru>
2021-04-09 22:34:24 +0300

    Added played column to player and reformatted the file

--- migrations/002-player-userdata-queue.sql
+++ migrations/002-player-userdata-queue.sql
@@ -1,22 +1,26 @@
 CREATE TABLE Queue (
-      id      INTEGER PRIMARY KEY AUTOINCREMENT,
-      musicId INTEGER REFERENCES Music (id)
-                          NOT NULL
-    );
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    musicId INTEGER REFERENCES Music (id)
+                    NOT NULL
+);
 
 CREATE TABLE Player (
-    shuffle         BOOLEAN NOT NULL,
-    repeat          INT NOT NULL     CHECK (repeat BETWEEN 0 AND 2),
-    playingMusicId  INT    REFERENCES Music (id),
-    playingFromType INTEGER CHECK (playingFromType >= 0),
+    shuffle         BOOLEAN        NOT NULL,
+    repeat          INT            NOT NULL
+                                   CHECK (repeat BETWEEN 0 AND 2),
+    playingMusicId  INT            REFERENCES Music (id),
+    playingFromType INTEGER        CHECK (playingFromType >= 0),
     playingFromId   INTEGER,
-    volume          DECIMAL(3, 2) NOT NULL CHECK (volume BETWEEN 0 AND 1)
+    played          DECIMAL (8, 7) NOT NULL
+                                   CHECK (played BETWEEN 0 AND 1),
+    volume          DECIMAL (3, 2) NOT NULL
+                                   CHECK (volume BETWEEN 0 AND 1)
 );
 
 CREATE TABLE UserData (
-    scanOnStart     BOOLEAN NOT NULL,
-    firstLaunch     BOOLEAN NOT NULL
+    scanOnStart BOOLEAN NOT NULL,
+    firstLaunch BOOLEAN NOT NULL
 );
 
-INSERT INTO Player(shuffle, repeat, volume) VALUES(0, 0, 1);
-INSERT INTO UserData(firstLaunch, scanOnStart) VALUES(1, 1);
+INSERT INTO Player(shuffle, repeat, played, volume) VALUES(false, false, 0, 1);
+INSERT INTO UserData(firstLaunch, scanOnStart) VALUES(true, true);
