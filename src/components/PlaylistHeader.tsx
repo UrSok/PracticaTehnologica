fe6d17012ad2fb4b145a0997423484bdcf19409b
Cătălina Cojocari <cojocaricatalin@gmail.com>
2021-04-11 20:29:15 +0300

    PlaylistHeader Component prototype

---
+++ src/components/PlaylistHeader.tsx
@@ -0,0 +1,48 @@
+import { Observer } from 'mobx-react';
+import React from 'react';
+import Button from './Button';
+
+interface Props {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  playlist?: Object;
+}
+
+const playlistEx = {
+  id: 1,
+  imageSource:
+    'https://github.com/morpheusthewhite/spicetify-themes/raw/master/Dribbblish/base.png',
+  title: 'My Shazam Tracks',
+  nrOfSongs: 18,
+  duration: '1 h 10 min', // the sum of all songs' duration
+};
+
+const PlaylistHeader: React.FC<Props> = () => {
+  // const { playlist } = this?.props;
+  // return playlist.map(() => {
+  const { id, imageSource, title, nrOfSongs, duration } = playlistEx;
+  return (
+    <div className="PlaylistHeader" key={id}>
+      <img className="HeaderImage" src={imageSource} alt="test" />
+      <div className="HeaderData">
+        <span className="HeaderLabel">PLAYLIST</span>
+        <h1 className="HeaderTitle">{title}</h1>
+        <span className="HeaderMetaInfo">
+          • {nrOfSongs} songs, {duration}
+        </span>
+
+        <div className="HeaderButtons">
+          <div className="HeaderButton">
+            <Button className="PlayButton" text="Play" onClick={() => {}} />
+          </div>
+
+          <div className="HeaderButton">
+            <Button className="MoreButton" text="•••" onClick={() => {}} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  // });
+};
+
+export default PlaylistHeader;
--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,48 +1,15 @@
 import React from 'react';
-import * as FiIcons from 'react-icons/fi';
 import * as BsIcons from 'react-icons/bs';
-import { ButtonsClassNames, PagesClassNames } from '../constants/ClassNames';
-import PageTitle from '../components/PageTitle';
-import IconButton from '../components/IconButton';
-import Button from '../components/Button';
+import { PagesClassNames } from '../constants/ClassNames';
 import Table from '../components/Table';
+import { useRootStore } from '../utils/StoreContext';
+import PlaylistHeader from '../components/PlaylistHeader';
 
 const Playlist: React.FC = () => {
+  const { musicStore } = useRootStore();
   return (
     <div className={PagesClassNames.Playlist}>
-      {/* <PageTitle PageName="Playlist" /> */}
-      <div className="PlaylistHeader">
-        <img
-          className="HeaderImage"
-          src="https://github.com/morpheusthewhite/spicetify-themes/raw/master/Dribbblish/base.png"
-          alt="test"
-        />
-        <div className="HeaderData">
-          <span className="HeaderLabel">PLAYLIST</span>
-          <h1 className="HeaderTitle">My Shazam Tracks</h1>
-          <span className="HeaderMetaInfo"> • 18 songs, 1 h 10 min</span>
-
-          <div className="HeaderButtons">
-            <div className="HeaderButton">
-              <Button
-                className="PlayButton"
-                text="Play"
-                // disabled
-                onClick={() => {}}
-              />
-            </div>
-
-            <div className="HeaderButton">
-              {/* <IconButton
-                className="MoreButton"
-                icon={<FiIcons.FiMoreHorizontal className="icon" />}
-                onClick={() => {}}
-              /> */}
-              <Button className="MoreButton" text="•••" onClick={() => {}} />
-            </div>
-          </div>
-        </div>
-      </div>
+      <PlaylistHeader />
 
       <div className="Input">
         <div className="Filter">
@@ -50,7 +17,7 @@ const Playlist: React.FC = () => {
           <input className="FilterInput" type="text" placeholder="Filter" />
         </div>
       </div>
-      {/* <Table musicList={MusicManager.instance.queue} /> */}
+      <Table musicList={musicStore?.musicList} />
     </div>
   );
 };
