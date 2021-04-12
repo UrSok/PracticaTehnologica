3cf85e53aaadb7327398860a62e835f6e7ecd01d
Cătălina Cojocari <cojocaricatalin@gmail.com>
2021-04-12 16:43:35 +0300

    Modified the Queue page

--- src/pages/Queue.tsx
+++ src/pages/Queue.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
+import Table from '../components/Table';
 import { PagesClassNames } from '../constants/ClassNames';
+import { useRootStore } from '../utils/StoreContext';
 
 const Queue: React.FC = () => {
+  const { musicStore } = useRootStore();
+
   return (
     <div>
       <div className={PagesClassNames.Queue}>
-        <PageTitle PageName="Queue" />
-        <div className="Card">
-          <div>
-            <img
-              width="250"
-              height="250"
-              src="https://github.com/morpheusthewhite/spicetify-themes/raw/master/Dribbblish/base.png"
-              alt="test"
-            />
-          </div>
+        {/* <PageTitle PageName="Play Queue" /> */}
+        <h1 className="QueueHeader">Play Queue</h1>
+
+        <div className="SectionDivider">
+          <span>Now Playing</span>
         </div>
+
+        <Table
+          musicList={musicStore?.musicList.filter((_value, index) => {
+            return index === 1;
+          })}
+        />
+
+        <div className="SectionDivider">
+          <span>Next Up</span>
+        </div>
+
+        <Table
+          musicList={musicStore?.musicList.filter((_value, index) => {
+            return index > 1;
+          })}
+        />
       </div>
     </div>
   );
